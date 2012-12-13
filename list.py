91d75b3255ef50c637aa735193fa2b211b0185c1
dstenb <dstenb@gmail.com>
2012-12-13 18:39:40 +0100

    added missing files

---
+++ list.py
@@ -0,0 +1,56 @@
+class List(object):
+
+    def __init__(self, items=[], listeners=[]):
+        self.items = items
+        self.listeners = listeners
+
+    def __getitem__(self, index):
+        return self.items[index]
+
+    def __len__(self):
+        return len(self.items)
+
+    def _notify(self):
+        for o in self.listeners:
+            o.list_changed(self)
+
+    def add_listener(self, o):
+        self.listeners.append(o)
+
+    def set(self, items):
+        self.items = items
+        self._notify()
+
+
+class Path(object):
+
+    def __init__(self):
+        self.list = [ ]
+
+    def push(self, s):
+        self.list.append(s)
+
+    def pop(self):
+        if len(self.list) > 0:
+            del self.list[-1]
+
+    def __str__(self):
+        return "/".join(self.list)
+
+
+class Browser(List):
+
+    def __init__(self):
+        super(Browser, self).__init__(["Albums", "Singles", "Mixes"])
+
+
+class Playlist(List):
+
+    def __init__(self):
+        super(Playlist, self).__init__()
+        self.version = 0
+
+    def set(self, items, version):
+        self.items = items
+        self.version = version
+        self._notify()
---
+++ wrapper.py
@@ -0,0 +1,76 @@
+from mpd import (MPDClient, CommandError)
+from socket import error as SocketError
+
+
+class Changes:
+
+    def __init__(self):
+        self.changes = []
+
+    def add(self, _list):
+        for s in _list:
+            if not s in self.changes:
+                self.changes.append(s)
+
+    def get(self):
+        c = self.changes
+        self.changes = []
+        return c
+
+
+class MPDWrapper():
+
+    def __init__(self, host, port):
+        self.mpd = MPDClient(use_unicode=True)
+        self.changes = Changes()
+        self.host = host
+        self.port = port
+        self.connected = False
+        self.in_idle = False
+
+    def connect(self):
+        try:
+            self.mpd.connect(self.host, self.port)
+            self.connected = True
+        except SocketError:
+            self.connected = False
+        return self.connected
+
+    def disconnect(self):
+        if self.connected:
+            self.mpd.disconnect()
+            self.connected = False
+
+    def fileno(self):
+        return self.mpd.fileno()
+
+    def get_changes(self):
+        return self.changes.get() if not self.in_idle else []
+
+    def idle(self):
+        if self.connected and not self.in_idle:
+            self.in_idle = True
+            self.mpd.send_idle()
+        return self.in_idle
+
+    def noidle(self, block=False):
+        changes = []
+        if self.connected and self.in_idle:
+            self.in_idle = False
+            if not block:
+                self.mpd.send_noidle()
+            changes = self.mpd.fetch_idle()
+            self.changes.add(changes)
+
+    def playlist(self):
+        return self.mpd.playlistinfo()
+
+    def player(self, name, *args):
+        if self.connected:
+            self.noidle()
+            getattr(self.mpd, name)(*args)
+
+    def status(self):
+        return self.mpd.status()
+
+
